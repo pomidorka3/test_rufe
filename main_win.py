52f1237faa22c05fe3d13e5bb9748e794cd43c9b
aaaaaaaaaaa <132383458+pomidorka3@users.noreply.github.com>
2023-05-06 15:57:56 +0300

    Update main_win.py

--- main_win.py
+++ main_win.py
@@ -1,1 +1,25 @@
-
+class MainWin(QWidget):
+    def __init__(self):
+        super.__init__()
+        self.initUI()
+        self.connects()
+        self.set_appear()
+        self.show()
+    def initUI(self):
+        self.btn_next = QPushButton(txt_next,self)
+        self.hello_text = QLabel(txt_hello)
+        self.instraction = QLabel(txt_instraction)
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text, alignment= Qt.AllignLeft)
+        self.layout_line.addWidget(self.instraction, alignment= Qt.AllignLeft)
+        self.layout_line.addWidget(self.btn_next, alignment= Qt.AllignCentr)
+        self.setLayout(self.layout_line)
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
